69f90203ab7683c73680e35d9a1587c6688cb3ac
Noel Deguzman <noel.deguzman@me.com>
2015-06-09 22:21:14 -0700

    AddressBloc Search Checkpoint

--- models/address_book.rb
+++ models/address_book.rb
@@ -34,4 +34,30 @@ class AddressBook
 
     return csv.count
   end
+
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+ # #1
+     lower = 0
+     upper = entries.length - 1
+
+ # #2
+     while lower <= upper
+ # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+ # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+    return nil
+  end
+
 end
